43dddc7bbe8928e8e7e9ee5f1aa6768aa34b84d5
jmartinez729 <jmartinez729@gmail.com>
2019-01-24 11:34:37 -0500

    added css including snake imgs & score

--- games_frontend/src/index.js
+++ games_frontend/src/index.js
@@ -29,6 +29,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const userNameInput = document.querySelector('#userInputContainer input')
     const scoresData = document.querySelector('#scoresData')
     const gameCanvas = document.getElementById("gameCanvas")
+    const duckImage = document.querySelector('#duck')
+    const headImage = document.querySelector('#icon')
 
     // returns a two-dimensional drawing context
     const ctx = gameCanvas.getContext("2d");
@@ -44,6 +46,7 @@ document.addEventListener('DOMContentLoaded', function() {
       {x: 120, y: 150},
       {x: 110, y: 150}
     ]
+    
     // The user's score
     let score = 0;
     // When set to true the snake is changing direction
@@ -112,8 +115,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
       gameContainer.addEventListener('click', function(event) {
       if (event.target.id === "startGame") {
-        console.log(GAME_SPEED)
-        console.log(dx, dy)
+
 
         snake = [
           {x: 150, y: 150},
@@ -198,21 +200,18 @@ document.addEventListener('DOMContentLoaded', function() {
         scoreContainer.style.display = "none"
         finalScore.innerText = score
 
-        // gamesList.innerHTML += renderOneGame
       } else {
 
-
-
-      setTimeout(function onTick() {
-        changingDirection = false;
-        clearCanvas();
-        drawFood();
-        advanceSnake();
-        drawSnake();
-        // Call game again
-        main();
-      }, GAME_SPEED)
-    }
+        setTimeout(function onTick() {
+          changingDirection = false;
+          clearCanvas();
+          drawFood();
+          advanceSnake();
+          drawSnake();
+          // Call game again
+          main();
+        }, GAME_SPEED)
+      }
     }
 
 
@@ -220,10 +219,6 @@ document.addEventListener('DOMContentLoaded', function() {
      * Change the background colour of the canvas to CANVAS_BACKGROUND_COLOR and
      * draw a border around it
      */
-
-
-
-
     function clearCanvas() {
       //  Select the colour to fill the drawing
       ctx.fillStyle = CANVAS_BACKGROUND_COLOR;
@@ -234,15 +229,16 @@ document.addEventListener('DOMContentLoaded', function() {
       // Draw a "border" around the entire canvas
       ctx.strokeRect(0, 0, gameCanvas.width, gameCanvas.height);
     }
+
+
     /**
      * Draw the food on the canvas
      */
     function drawFood() {
-      ctx.fillStyle = FOOD_COLOR;
-      ctx.strokestyle = FOOD_BORDER_COLOR;
-      ctx.fillRect(foodX, foodY, 10, 10);
-      ctx.strokeRect(foodX, foodY, 10, 10);
+      ctx.drawImage(duckImage, foodX, foodY, 10, 10)
     }
+
+
     /**
      * Advances the snake by changing the x-coordinates of its parts
      * according to the horizontal velocity and the y-coordinates of its parts
@@ -260,7 +256,10 @@ document.addEventListener('DOMContentLoaded', function() {
         // Increase score
         score += 10;
         // Display score on screen
-        scoreContainer.innerHTML = score;
+        scoreContainer.innerHTML = `
+        <h2><u>Score</u></h2>
+        <p>${score}</p>
+        `
         // Generate new food location
         createFood();
       } else {
@@ -268,6 +267,8 @@ document.addEventListener('DOMContentLoaded', function() {
         snake.pop();
       }
     }
+
+
     /**
      * Returns true if the head of the snake touched another part of the game
      * or any of the walls
@@ -282,6 +283,8 @@ document.addEventListener('DOMContentLoaded', function() {
       const hitTopWall = snake[0].y > gameCanvas.height - 10;
       return hitLeftWall || hitRightWall || hitTopWall || hitBottomWall
     }
+
+
     /**
      * Generates a random number that is a multiple of 10 given a minumum
      * and a maximum number
@@ -291,6 +294,8 @@ document.addEventListener('DOMContentLoaded', function() {
     function randomTen(min, max) {
       return Math.round((Math.random() * (max-min) + min) / 10) * 10;
     }
+
+
     /**
      * Creates random set of coordinates for the snake food.
      */
@@ -305,28 +310,28 @@ document.addEventListener('DOMContentLoaded', function() {
         if (foodIsoNsnake) createFood();
       });
     }
+
+
     /**
      * Draws the snake on the canvas
      */
     function drawSnake() {
-      // loop through the snake parts drawing each part on the canvas
       snake.forEach(drawSnakePart)
     }
+
     /**
      * Draws a part of the snake on the canvas
      * @param { object } snakePart - The coordinates where the part should be drawn
      */
     function drawSnakePart(snakePart) {
-      // Set the colour of the snake part
-      ctx.fillStyle = SNAKE_COLOR;
-      // Set the border colour of the snake part
-      ctx.strokestyle = SNAKE_BORDER_COLOR;
-      // Draw a "filled" rectangle to represent the snake part at the coordinates
-      // the part is located
-      ctx.fillRect(snakePart.x, snakePart.y, 10, 10);
-      // Draw a border around the snake part
-      ctx.strokeRect(snakePart.x, snakePart.y, 10, 10);
+
+      if (snakePart === snake[0]) {
+        ctx.drawImage(headImage, snakePart.x, snakePart.y, 10, 10)
+      } else {
+        ctx.drawImage(duckImage, snakePart.x, snakePart.y, 10, 10)
+      }
     }
+
     /**
      * Changes the vertical and horizontal velocity of the snake according to the
      * key that was pressed.
@@ -335,6 +340,8 @@ document.addEventListener('DOMContentLoaded', function() {
      * For example if the the direction is 'right' it cannot become 'left'
      * @param { object } event - The keydown event
      */
+
+
     function changeDirection(event) {
       const LEFT_KEY = 37;
       const RIGHT_KEY = 39;
@@ -394,10 +401,6 @@ document.addEventListener('DOMContentLoaded', function() {
 
   mySound = new sound("/Users/johnmartinez/dev/js/api-test/snake-api/app/images/quack.mp3");
 
-  const buttonForQuack = document.querySelector('#sound')
-  buttonForQuack.addEventListener('click', function(event) {
-    mySound.play();
-  })
 
 ////////////////////////// END OF SOUNDS //////////////////////////
 
